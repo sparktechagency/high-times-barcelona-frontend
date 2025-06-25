4bcac8f1a051fe3c3629547efb6dd4fdd82786f1
Nadir Hossain <nadirhossain336@gmail.com>
2025-06-25 09:09:47 +0600

    bugs: :bugs: fix

--- src/redux/base/baseApi.ts
+++ src/redux/base/baseApi.ts
@@ -2,8 +2,8 @@ import { BaseQueryApi, BaseQueryFn, createApi, DefinitionType, FetchArgs, fetchB
 import { RootState } from '../store';
 import { logout, setUser } from '../features/auth/authSlice';
 
-// export const backendUrl = 'https://apuserver.binarybards.online';
-export const backendUrl = 'http://37.148.204.85:5000';
+export const backendUrl = 'https://api.weedinbarcelona.com';
+// export const backendUrl = 'http://37.148.204.85:5000';
 const baseQuery = fetchBaseQuery({
       baseUrl: `${backendUrl}/api/v1`,
       credentials: 'include',
